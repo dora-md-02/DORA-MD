142b4724ebc28e898294f591df306b2d13271274
dora-md-02 <ircdora397@gmail.com>
2025-04-06 21:50:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "DORA-MD~toJlBSya#klyUwrJ6h8xN0Z1Q0Qg2eM8l0bazxqxxBueWEaC2zgQ",
 ALIVE_IMG: process.env.ALIVE_IMG || "",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am DORA MD I am alive now!",
 };
